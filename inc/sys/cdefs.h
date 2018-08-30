d0caa09596fe4ac9a4d769f4a8402218c3a26af7
sezero <sezero@users.sourceforge.net>
2018-08-30 08:20:20 +0300

    sys/cdefs.h: use include_next for djgpp.

--- inc/sys/cdefs.h
+++ inc/sys/cdefs.h
@@ -44,9 +44,8 @@
 #ifndef __SYS_CDEFS_H
 #define __SYS_CDEFS_H
 
-#if defined(__DJGPP__) && !defined(djgpp_cdefs_incl)
-  #include "/dev/env/DJDIR/include/sys/cdefs.h"
-  #define djgpp_cdefs_incl
+#if defined(__DJGPP__)
+  #include_next <sys/cdefs.h>
 #endif
 
 #ifdef __cplusplus
