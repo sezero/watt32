cc9838e08976b80c999eb9ceeeff316e7aa816f4
Gisle Vanem <gisle.vanem@gmail.com>
2020-03-25 15:00:36 +0000

    Added colour support for AppVeyor jobs. Uses 'c:\msys64\usr\bin\echo.exe' for yellow and red messages.

--- appveyor-script.bat
+++ appveyor-script.bat
@@ -6,6 +6,10 @@ prompt $P$G
 :: 'APPVEYOR_PROJECT_NAME==Watt-32' unless testing this "appveyor-script.bat [build_src | build_bin | build_tests]"
 :: locally using 'cmd'.
 ::
+:: Change this for an 'echo.exe' with colour support. Like Cygwin.
+::
+set _ECHO=%MSYS2_ROOT%\usr\bin\echo.exe -e
+
 if %APPVEYOR_PROJECT_NAME%. == . (
   if %BUILDER%. == . (
     echo BUILDER not set!
@@ -17,11 +21,13 @@ if %APPVEYOR_PROJECT_NAME%. == . (
   )
   set APPVEYOR_BUILD_FOLDER=%WATT_ROOT%
   set APPVEYOR_BUILD_FOLDER_UNIX=e:/net/watt
+
 ) else (
   set APPVEYOR_BUILD_FOLDER_UNIX=c:/projects/Watt-32
+  set _ECHO=c:\msys64\usr\bin\echo.exe -e
 )
 
-echo Doing '%1' for BUILDER=%BUILDER%.
+%_ECHO% "\e[1;33mDoing '%1' for BUILDER=%BUILDER%.\e[0m"
 
 ::
 :: Stuff common to '[build_src | build_bin | build_tests]'
@@ -108,53 +114,56 @@ set BITS=32
 if %CPU%. == x64. set BITS=64
 
 if %USES_CL%. == 1. (
-  echo Generating src\oui-generated.c.
+  %_ECHO% "\e[1;33mGenerating src\oui-generated.c.\e[0m"
   python.exe make-oui.py > oui-generated.c
   if ERRORLEVEL 0 set CL=-DHAVE_OUI_GENERATATED_C
   echo --------------------------------------------------------------------------------------------------
 )
 
+%_ECHO% "\e[1;33mcall configur.bat %BUILDER%:\e[0m"
+
 if %BUILDER%. == visualc. (
   call configur.bat visualc
   echo Building release for %CPU%.
+  %_ECHO% "\e[1;33mBuilding release for %CPU%:\e[0m"
   nmake -nologo -f visualc-release_%BITS%.mak
   exit /b
 )
 
 if %BUILDER%. == clang. (
   call configur.bat clang
-  echo Building for %CPU%.
+  %_ECHO% "\e[1;33mBuilding release for %CPU%:\e[0m"
   make -f clang-release_%BITS%.mak
   exit /b
 )
 
 if %BUILDER%. == mingw32. (
   call configur.bat mingw32
-  echo Building for x86 only.
+  %_ECHO% "\e[1;33mBuilding for x86 only:\e[0m"
   make -f MinGW32.mak
   exit /b
 )
 
 if %BUILDER%. == mingw64. (
   call configur.bat mingw64
-  echo Building for %CPU%.
+  %_ECHO% "\e[1;33mBuilding for %CPU%:\e[0m"
   make -f MinGW64_%BITS%.mak
   exit /b
 )
 
 if %BUILDER%. == djgpp. (
   if not exist %DJGPP%\bin\i586-pc-msdosdjgpp-gcc.exe (
-    echo Downloading Andrew Wu's DJGPP cross compiler.
+    %_ECHO% "\e[1;33mDownloading Andrew Wu's DJGPP cross compiler:\e[0m"
     curl -O -# http://www.watt-32.net/CI/dj-win.zip
     if not errorlevel == 0 (
-      echo The curl download of http://www.watt-32.net/CI/dj-win.zip failed!
+      %_ECHO% "\e[1;31mThe curl download of http://www.watt-32.net/CI/dj-win.zip failed!\e[0m"
       exit /b
     )
     7z x -y -o%DJGPP% dj-win.zip > NUL
     rm -f dj-win.zip
   )
   call configur.bat djgpp
-  echo Building for djgpp.
+  %_ECHO% "\e[1;33mBuilding for djgpp:\e[0m"
   make -f djgpp.mak
   exit /b
 )
@@ -162,21 +171,21 @@ if %BUILDER%. == djgpp. (
 if %BUILDER%. == watcom. (
   if not exist %WATCOM%\binnt\wmake.exe (
     mkdir %WATCOM%
-    echo Downloading OpenWatcom 2.0.
+    %_ECHO% "\e[1;33mDownloading OpenWatcom 2.0:\e[0m"
     curl -o %WATCOM_ZIP% -# http://www.watt-32.net/CI/watcom20.zip
     if not errorlevel == 0 (
-      echo The curl download of http://www.watt-32.net/CI/watcom20.zip failed!
+      %_ECHO% "\e[1;31mThe curl download of http://www.watt-32.net/CI/watcom20.zip failed!\e[0m"
       exit /b
     )
     7z x -y -o%WATCOM% %WATCOM_ZIP% > NUL
   )
   call configur.bat watcom
-  echo Building for Watcom/Win32.
+  %_ECHO% "\e[1;33mBuilding for Watcom/Win32:\e[0m"
   wmake -f watcom_w.mak
   exit /b
 )
 
-echo Illegal BUILDER / CPU (BUILDER=%BUILDER%, CPU=%CPU%) values! Remember cmd.exe is case-sensitive.
+%_ECHO% "\e[1;31mIllegal BUILDER / CPU (BUILDER=%BUILDER%, CPU=%CPU%) values! Remember cmd.exe is case-sensitive.\e[0m"
 exit /b 1
 
 ::
@@ -185,7 +194,7 @@ exit /b 1
 :build_bin
 
 if %CPU%. == x64. (
-  echo No build_bin for x64 yet.
+  %_ECHO% "\e[1;31mNo build_bin for x64 yet.\e[0m"
   exit /b 0
 )
 
@@ -205,24 +214,24 @@ set PROGS_WC=ping.exe htget.exe finger.exe tcpinfo.exe con-test.exe ^
 
 cd bin
 if %BUILDER%. == djgpp. (
-  echo Building PROGS_DJ=%PROGS_DJ%
+  %_ECHO% "\e[1;33mBuilding PROGS_DJ=%PROGS_DJ%:\e[0m"
   make -f djgpp_win.mak DPMI_STUB=0 %PROGS_DJ%
   exit /b
 )
 
 if %BUILDER%. == visualc. (
-  echo Building PROGS_VC=%PROGS_VC%
+  %_ECHO% "\e[1;33mBuilding PROGS_VC=%PROGS_VC%:\e[0m"
   nmake -nologo -f visualc.mak %PROGS_VC%
   exit /b
 )
 
 if %BUILDER%. == watcom. (
-  echo Building PROGS_WC=%PROGS_WC%
+  %_ECHO% "\e[1;33mBuilding PROGS_WC=%PROGS_WC%:\e[0m"
   wmake -f wc_win.mak %PROGS_WC%
   exit /b
 )
 
-echo No 'build_bin' for 'BUILDER=%BUILDER%' yet.
+%_ECHO% "\e[1;31mNo 'build_bin' for 'BUILDER=%BUILDER%' yet.\e[0m"
 exit /b 0
 
 ::
@@ -230,6 +239,6 @@ exit /b 0
 ::
 :build_tests
 cd src\tests
-echo Test for %BUILDER% will come here later
+%_ECHO% "\e[1;33mWill do 'src/tests' for '%BUILDER%' later. Simply doing 'call configur.bat %BUILDER%' now.\e[0m"
 call configur.bat %BUILDER%
 exit /b 0
