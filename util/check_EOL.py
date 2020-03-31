077c6cff39c8f4a33fd4a3a6bd7f3045a71c5873
Gisle Vanem <gisle.vanem@gmail.com>
2020-03-31 18:43:00 +0000

    Get rid of all 'global'. Add '-v' for trace.

--- util/check_EOL.py
+++ util/check_EOL.py
@@ -20,28 +20,30 @@ import sys, os, glob, string, fnmatch, argparse
 parser = argparse.ArgumentParser (add_help=False)
 parser.add_argument ("-h", dest="help", action="store_true")
 parser.add_argument ("-r", dest="recursive", action="store_true")
+parser.add_argument ("-v", dest="verbose", action="count")
 parser.add_argument ("spec", nargs='?')
 parser.add_argument ("subdir", nargs='?')
 
 opt = parser.parse_args()
 
-total_binary = total_EOL_unix = total_EOL_dos = total_files = 0
+def trace (level, s):
+  if opt.verbose >= level:
+    print (s)
 
-def check_EOL (file):
+def check_file (file, tot):
   EOL_dos = EOL_unix = bin_chars = 0
   pos = flen = is_bin = ascii_chars = 0
-
-  global total_files
-  total_files += 1
 
   with open(file, 'rb') as infile:
     content = infile.read()
     flen = len(content)
     if flen <= 1:
+      tot ['unknown'] += 1
       return
+
     while 1:
-      c0 = content[pos]
-      c1 = content[pos+1]
+      c0 = content [pos]
+      c1 = content [pos+1]
       if not str(c0) in string.printable:
         bin_chars += 1
       else:
@@ -51,32 +53,36 @@ def check_EOL (file):
         c1_0A = (c1 == '\n')
         c1_0D = (c1 == '\r')
 
-        if c0_0D and c1_0A:       # \r\n
+        if c0_0D and c1_0A:        # \r\n
           EOL_dos += 1
           pos     += 1
-        elif c0_0A and not c1_0D: # \n
+        elif c0_0A and not c1_0D:  # \n
           EOL_unix += 1
 
       pos += 1
       if bin_chars > ascii_chars:
         is_bin = 1
-      if is_bin or pos >= flen/10:
+
+      if is_bin or pos >= flen or pos >= max(100,flen/10):
         break
 
   if is_bin:
     print ("Binary:   %s" % file)
-    global total_binary
-    total_binary += 1
+    tot['binary'] += 1
+
   elif EOL_unix > 0 and EOL_dos == 0:
     print ("Unix EOL: %s" % file)
-    global total_EOL_unix
-    total_EOL_unix += 1
+    tot['EOL_unix'] += 1
+
   elif EOL_unix == 0 and EOL_dos > 0:
     print ("DOS EOL:  %s" % file)
-    global total_EOL_dos
-    total_EOL_dos += 1
+    tot['EOL_dos'] += 1
 
-def process_dir (d):
+  else:
+    tot['unknown'] += 1
+    print ("Unknown:  %s" % file)
+
+def process_dir (d, tot):
   cwd = os.getcwd()
   try:
     os.chdir(d)
@@ -88,9 +94,13 @@ def process_dir (d):
       file = ("%s/%s" % (os.getcwd(), in_file)).replace ('\\','/')
       if os.path.isdir(file):
         if opt.recursive:
-          process_dir (file)
+          process_dir (file, tot)
       elif fnmatch.fnmatch(in_file, opt.spec):
-        check_EOL (file)
+        tot ['files'] += 1
+        check_file (file, tot)
+      else:
+        trace (2, "skipping %s" % in_file)
+
   os.chdir (cwd)
 
 #
@@ -104,16 +114,23 @@ opt.spec = opt.spec.replace ('\\','/')
 if opt.spec.endswith('/'):
   opt.spec += '*'
 
-opt.subdir = os.path.split(opt.spec)[0]
-opt.spec   = os.path.split(opt.spec)[1]
+opt.subdir = os.path.split (opt.spec)[0]
+opt.spec   = os.path.split (opt.spec)[1]
 if opt.subdir == '':
   opt.subdir = '.'
 
-process_dir (opt.subdir)
+totals = { 'files'    : 0,
+           'binary'   : 0,
+           'unknown'  : 0,
+           'EOL_dos'  : 0,
+           'EOL_unix' : 0 }
 
-print ("total_binary:   %3d" % total_binary)
-print ("total_EOL_unix: %3d" % total_EOL_unix)
-print ("total_EOL_dos:  %3d" % total_EOL_dos)
-print ("total_files:    %3d" % total_files)
+trace (1, "Searching, opt.subdir: '%s', opt.spec: '%s'" % (opt.subdir, opt.spec))
 
+process_dir (opt.subdir, totals)
 
+print ("total_binary:   %3d" % totals['binary'])
+print ("total_EOL_unix: %3d" % totals['EOL_unix'])
+print ("total_EOL_dos:  %3d" % totals['EOL_dos'])
+print ("total_unknown:  %3d" % totals['unknown'])
+print ("total_files:    %3d" % totals['files'])
