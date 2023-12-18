37e495d0cb6e4f8267ef76b8626db2be16ddcc29
Gisle Vanem <gvanem@yahoo.no>
2023-12-18 16:18:41 +0100

    Need GNU-make for 'highc.mak'.

--- src/all_dos.bat
+++ src/all_dos.bat
@@ -3,10 +3,10 @@ if not %1. == clean. call configur.bat all
 if %1. == clean.     call configur.bat clean
 
 echo ---- running djgpp.mak %1 ---- > all_dos.log
-make  -f djgpp.mak %1 >&> all_dos.log
+make -f djgpp.mak %1 >&> all_dos.log
 
 echo ---- running highc.mak %1 ---- >> all_dos.log
-maker -f highc.mak %1  >> all_dos.log
+make -f highc.mak %1 >> all_dos.log
 
 echo ---- running watcom_s.mak %1 ---- >> all_dos.log
 wmake -h -f watcom_s.mak %1 >> all_dos.log
